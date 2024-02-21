ad2014af7a06f8f751b25e723e10d7f2d589f473
Vlad Shu1t3 <vnuk9292@icloud.com>
2024-02-22 00:30:21 +0300

    Обновил код

--- telegram_webp.py
+++ telegram_webp.py
@@ -1,4 +1,5 @@
 import os
+from io import BytesIO
 import asyncio
 
 from PIL import Image
@@ -6,8 +7,7 @@ from PIL import Image
 from aiogram import F
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from aiogram.types import FSInputFile
-
+from aiogram.types import BufferedInputFile
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,74 +15,52 @@ load_dotenv()
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
-processing_done = False
-complete_img = []
 
 
-def check_and_create_dir():
-    directories = ['img_in', 'out']
-
-    for dir in directories:
-        os.makedirs(os.path.join(os.getcwd(), dir), exist_ok=True)
-
-
-async def convert(pth_in='img_in'):
-    for pic in os.listdir(path=pth_in):
-        if pic.endswith('.jpg') or pic.endswith('.png'):
-            img_path = os.path.join(pth_in, pic)
-            img = Image.open(img_path)
-            img = img.resize((512, 512))
-            f = os.path.splitext(pic)[0]
-            out_path = os.path.join('out', f'{f}.webp')
-
-            # Устанавливаем качество изображения для сжатия (от 0 до 100, где 0 - минимальное качество, 100 - максимальное)
-            quality = 95
-
-            # Сжимаем изображение и сохраняем в формате WebP
-            img.save(out_path, 'WEBP', quality=quality)
-
-            # Проверяем размер файла и уменьшаем качество, пока не уложимся в 64 КБ
-            while os.path.getsize(out_path) > 64 * 1024:  # переводим килобайты в байты
-                quality -= 1
-                img.save(out_path, 'WEBP', quality=quality)
-
-            print(f'Конвертация {pic} завершена и сохранена как {f}.webp. Размер файла: {os.path.getsize(out_path) / 1024} КБ')
-
-
-async def del_img(pth_in='img_in', pth_out='out'):
-    for file in os.listdir(path=pth_in):
-        os.remove(f'{pth_in}/{file}')
-    for file in os.listdir(path=pth_out):
-        os.remove(f'{pth_out}/{file}')
+async def convert(io_data: BytesIO) -> BytesIO:
+    # Чтение байтов данных из объекта BytesIO
+    pic = io_data.read()
+    # Открываем изображение из байтовых данных и меняем размер и качество
+    img = Image.open(BytesIO(pic))
+    img = img.resize((512, 512))
+    quality = 95
+    out = BytesIO()
+    # Сжимаем изображение и сохраняем в формате WebP
+    img.save(out, 'WEBP', quality=quality)
+    # Проверяем размер файла и уменьшаем качество, пока не уложимся в 64 КБ
+    while out.tell() > 64 * 1024:  # Проверяем размер данных в BytesIO объекте, если много уменьшаем
+        quality -= 1
+        # Сбрасываем указатель записи в начало объекта BytesIO и очищаем данные в объекте BytesIO
+        out.seek(0)
+        out.truncate(0)
+        img.save(out, 'WEBP', quality=quality)
+    # Возвращаем указатель чтения в начало объекта BytesIO
+    out.seek(0)
+    return out
 
 
 @dp.message(Command('start'))
 async def start_cmd(message: types.Message):
-    await message.answer("Отправьте изображения")
+    await message.answer('Отправьте изображения. Чем "квадратней", тем лучше! :)')
 
-# Функция сохранения отправленных фото
+
 @dp.message(F.photo)
 async def get_photo_conver_webp(message: types.Message, bot: Bot):
-    global processing_done
+    # Получаем ID фотки в самом лучше качестве
+    photo = message.photo[-1].file_id
 
-    if not processing_done:
-        file_info = await bot.get_file(message.photo[-1].file_id)
-        processing_done = True
-        file_name = file_info.file_path.split('photos/')[1]
-        await bot.download_file(file_info.file_path, 'img_in/' + file_name)
+    if photo:
+        # Этот блок получает фотку, путь для скачивания, скачивает и помещает в память
+        get_photo = await bot.get_file(photo)
+        photo_path = get_photo.file_path
+        downloaded_photo = await bot.download_file(photo_path, BytesIO())
+
         await message.answer("Обрабатываю изображения, подождите...")
-        await convert()
+        # Этот блок конвертирует изображение и генерирует объект для отправки
+        converted_photo = await convert(downloaded_photo)
+        input_file = BufferedInputFile(converted_photo.getvalue(), filename='image.webp')
         # Отправляем готовый шаблон-стикер
-        async def upload_doc():
-            for file_name in os.listdir(path='out/'):
-                if file_name not in complete_img:
-                    complete_img.append(file_name)
-                    webp_img = FSInputFile(f'out/{file_name}')
-                    await message.answer_document(webp_img, caption='Готовый WebP')
-        await upload_doc()
-        await del_img()
-        complete_img.clear()
-        processing_done = False
+        await message.answer_document(input_file, caption='Готовый WebP')
 
 
 async def main():
@@ -90,5 +68,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    check_and_create_dir()
     asyncio.run(main())
